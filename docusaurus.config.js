25f06080edb9759ed31cb0fc72e1bcc2bb063d47
Heloise-arc <heloise@buffer.finance>
2023-11-28 18:00:07 +0530

    buffer docs (docusaurus). docs only mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/logos/side-bar-logo.png',
 
   // Set the production url of your site here
-  url: 'https://buffer.finance/',
+  url: 'https://docs.buffer.finance/',
   // Set the /<baseUrl>/ pathname under which your site is served
   baseUrl: '/',
 
